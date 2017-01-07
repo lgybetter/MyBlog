062c7a6e75a8c587c1b14a4492072186f10c3aea
Guangyu Lin <linguangyu@bbcloud.com>
2017-01-07 23:12:15 +0800

    test
    Change-Id: I78d619ab9bdf36623ce973d7376ae2f1b43f0c84

--- models/user.js
+++ models/user.js
@@ -10,9 +10,9 @@ function User(user) {
 
 module.exports = User;
 
-User.prototype.save = function(callback) {
-	var md5 = crypto.createHash('md5'),email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
-	head ="http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
+User.prototype.save = function (callback) {
+	var md5 = crypto.createHash('md5'), email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+		head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
 	//要存入数据库的用户文档
 	var user = {
 		name: this.name,
@@ -21,115 +21,127 @@ User.prototype.save = function(callback) {
 		head: head
 	};
 	//打开数据库
-	mongodb.open(function(err,db) {
-		if(err) {
+	mongodb.open(function (err, db) {
+		if (err) {
 			return callback(err);//错误，返回err信息
 		}
-		//读取 users集合
-		db.collection('users' , function(err,collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);
-			}
-			//将用户数据插入users集合
-			collection.insert(user ,{
-				safe: true
-			}, function(err,user) {
-				mongodb.close();
-				if(err) {
+		db.authenticate('user', 'lgylgy', (err, result) => {
+			//读取 users集合
+			db.collection('users', function (err, collection) {
+				if (err) {
+					mongodb.close();
+					return callback(err);
+				}
+				//将用户数据插入users集合
+				collection.insert(user, {
+					safe: true
+				}, function (err, user) {
+					mongodb.close();
+					if (err) {
+						return callback(err);//错误，返回err信息
+					}
+					callback(null, user[0]);//成功 ，err为null，并返回存储后的用户文档
+				});
+			});
+		})
+
+	});
+};
+
+User.get = function (name, callback) {
+	//打开数据库
+	mongodb.open(function (err, db) {
+		if (err) {
+			return callback(err);//错误，返回err信息
+		}
+		db.authenticate('user', 'lgylgy', (err, result) => {
+			//读取users集合
+			db.collection('users', function (err, collection) {
+				if (err) {
+					mongodb.close();
 					return callback(err);//错误，返回err信息
 				}
-				callback(null , user[0]);//成功 ，err为null，并返回存储后的用户文档
+				//查找用户名（name键）值为name的一个文档
+				collection.findOne({
+					name: name
+				}, function (err, user) {
+					mongodb.close();
+					if (err) {
+						return callback(err);//失败，返回err信息
+					}
+					callback(null, user);//成功，返回查询的用户信息
+				});
 			});
-		});
+		})
+
 	});
 };
 
-User.get = function(name ,callback) {
+User.getAll = function (name, callback) {
 	//打开数据库
-	mongodb.open(function(err,db) {
-		if(err) {
-			return callback(err);//错误，返回err信息
-		}
-		//读取users集合
-		db.collection('users' , function(err , collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);//错误，返回err信息
-			}
-			//查找用户名（name键）值为name的一个文档
-			collection.findOne({
-				name: name
-			},function(err,user) {
-				mongodb.close();
-				if(err) {
-					return callback(err);//失败，返回err信息
-				}
-				callback(null,user);//成功，返回查询的用户信息
-			});
-		});
-	});
-};
-
-User.getAll = function(name,callback) {
-	//打开数据库
-	mongodb.open(function(err,db) {
-		if(err) {
+	mongodb.open(function (err, db) {
+		if (err) {
 			mongodb.close();
 			return callback(err);
 		}
-		//读取users 集合
-		db.collection('users',function(err,collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);//错误，返回err信息
-			}
-			var query = {};
-			if(name) {
-				query.name = name;
-			}
-			//根据qurey对象查询用户
-			collection.find(query).sort({
-				name: -1
-			}).toArray(function(err,users) {
-				mongodb.close();
-				if(err) {
-					return callback(err);
+		db.authenticate('user', 'lgylgy', (err, result) => {
+			db.collection('users', function (err, collection) {
+				if (err) {
+					mongodb.close();
+					return callback(err);//错误，返回err信息
 				}
-				callback(null,users);
+				var query = {};
+				if (name) {
+					query.name = name;
+				}
+				//根据qurey对象查询用户
+				collection.find(query).sort({
+					name: -1
+				}).toArray(function (err, users) {
+					mongodb.close();
+					if (err) {
+						return callback(err);
+					}
+					callback(null, users);
+				});
 			});
-		});
+		})
+		//读取users 集合
+
 	});
 }
 
-User.remove = function(name,callback) {
-	mongodb.open(function(err,db) {
-		if(err) {
+User.remove = function (name, callback) {
+	mongodb.open(function (err, db) {
+		if (err) {
 			return callback(err);
 		}
-		db.collection('users',function(err,collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.remove({
-				"name":name
-			},{
-				w:1
-			},function(err) {
-				// Post.remove(name,null,null,function(err) {
-				// 	if(err) {
-				// 		callback(err);
-				// 	}
-				// 	callback(null)
-				// });
-				mongodb.close();
-				if(err) {
+		db.authenticate('user', 'lgylgy', (err, result) => {
+			db.collection('users', function (err, collection) {
+				if (err) {
+					mongodb.close();
 					return callback(err);
 				}
-				callback(null);
+				collection.remove({
+					"name": name
+				}, {
+						w: 1
+					}, function (err) {
+						// Post.remove(name,null,null,function(err) {
+						// 	if(err) {
+						// 		callback(err);
+						// 	}
+						// 	callback(null)
+						// });
+						mongodb.close();
+						if (err) {
+							return callback(err);
+						}
+						callback(null);
+					});
 			});
-		});
+		})
+
 	});
 }
 
